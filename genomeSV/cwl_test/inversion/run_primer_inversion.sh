e8df80227c520705a2f111df0c3e41f4d70e2c2a
Dennis Kennetz <dennis.kennetz@stjude.org>
2019-04-17 13:53:43 -0500

    Removing genomeStandard and genomeSV directories, and made

--- genomeSV/cwl_test/inversion/run_primer_inversion.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH=$PATH:/home/dkennetz/primer3/src
-
-cwltool --preserve-entire-environment /home/dkennetz/PrimerPlex/Pybox/genomeSV/CWL_Pipelines/PrimerSV.cwl /home/dkennetz/PrimerPlex/Pybox/genomeSV/CWL_Pipelines/inversion.yml
