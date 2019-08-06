19e7833055b797183d2129bb7079beb97b1cb200
Dennis Kennetz <dennis.kennetz@stjude.org>
2019-08-06 16:52:21 -0500

    Revert "started changing bats to build yaml and added to setup.py. Also added cluster installer example."
    This reverts commit 398633113ff32a347391be4eec1d808c6839cc13.

--- cluster_install.sh
+++ cluster_install.sh
@@ -1,55 +1,32 @@
-#!/usr/bin/env bash
+cd ~
 
-echo "
-####################################################
-### Primer3 and In Silico PCR License Agreements ###
-####################################################
+mkdir bin
 
-Primer3 is licensed under GNU GPL v2.0. To see in detail with this license entails please visit:
-https://github.com/primer3-org/primer3/blob/master/LICENSE. 
-This message will serve as a user agreement to download primer3 as a completely independent software to PrimerTK. 
-PrimerTK does not serve to distribute primer3 or any of its components. By typing 'yes', you, the user, agree to the
-terms and conditions of the GPL2 license and are agreeing to download primer3 independently of PrimerTK. You can
-also refuse to download primer3 by entering no.
+wget https://hgwdev.gi.ucsc.edu/~kent/src/isPcr33.zip && unzip isPcr33.zip
 
-In Silico PCR [isPCR] is freely available for non-profit/educational use only without a license. 
-By typing 'yes' you are agreeing that you are a non-profit/educational institution and do not intend to use this
-commercially. We release rights with this installer to the user installing the software and take no responsibility
-of actions after install. 
+MACHTYPE=x86_64
+export MACHTYPE
 
-By Typing 'yes' it is serving as though you are personally installing the software, and inheriting all liability
-henceforth.
+mkdir ~/bin/$MACHTYPE
 
-[yes] signifies an agreement to download, [no] will stop the download and exit.
+PATH=$PATH:~/bin/$MACHTYPE
+export $PATH
 
-Please enter yes or no:"
+mkdir ./isPcrSrc/lib/$MACHTYPE
 
-read answer
+cd ./isPcrSrc/lib && make HG_WARN=""
 
-if [ $answer = "yes" ]; then
-    mydir=$(pwd)
-    git clone https://github.com/primer3-org/primer3.git primer3
-    cd ./primer3/src && make
-    make test
-    cd $mydir
-    wget https://hgwdev.gi.ucsc.edu/~kent/src/isPcr33.zip && unzip isPcr33.zip
-    MACHTYPE=x86_64
-    export MACHTYPE
-    mkdir ~/bin
-    mkdir ~/bin/$MACHTYPE
-    export PATH=$PATH:~/bin/$MACHTYPE
-    mkdir ./isPcrSrc/lib/$MACHTYPE
-    cd ./isPcrSrc/lib && make HG_WARN=""
-    cd ../ && make HG_WARN=""
-    cd ../
-    cp ~/bin/$MACHTYPE/isPcr .
-    rm -rf ~/bin/$MACHTYPE
-    rm -rf ./isPcrSrc/
-    rm -rf ./isPcr33.zip
-elif [ $answer = "no" ]; then
-    echo "User has declined licensing agreements. Exiting..."
-    exit 1
-else
-    echo "Please select an answer [yes or no]"
-    exit 1
-fi
+wait
+
+cd ../ && make HG_WARN=""
+
+cd ..
+
+cp ~/bin/$MACHTYPE/isPcr .
+
+rm -rf ~/bin/$MACHTYPE
+
+rm -rf ./isPcrSrc/
+
+rm -rf ./isPcr33.zip
+
