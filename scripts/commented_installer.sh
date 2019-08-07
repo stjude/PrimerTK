f6f7766cf9604a4390c95b65527c124c8481f637
Dennis Kennetz <dennis.kennetz@stjude.org>
2019-08-07 16:29:39 -0500

    Fixed some wording in the commented_installer echo statement.

--- scripts/commented_installer.sh
+++ scripts/commented_installer.sh
@@ -8,12 +8,11 @@ echo "
 Primer3 is licensed under GNU GPL v2.0. To see in detail with this license entails please visit:
 https://github.com/primer3-org/primer3/blob/master/LICENSE. 
 This message will serve as a user agreement to download primer3 as a completely independent software to PrimerTK. 
-PrimerTK does not serve to distribute primer3 or any of its components. By typing 'yes', you, the user, agree to the
-terms and conditions of the GPL2 license and are agreeing to download primer3 independently of PrimerTK. You can
-also refuse to download primer3 by entering no.
+PrimerTK does not serve to distribute primer3 or any of its components. By uncommenting, you, the user, agree to the
+terms and conditions of the GPL2 license and are agreeing to download primer3 independently of PrimerTK.
 
 In Silico PCR [isPCR] is freely available for non-profit/educational use only without a license. 
-By typing 'yes' you are agreeing that you are a non-profit/educational institution and do not intend to use this
+By uncommenting you are agreeing that you are a non-profit/educational institution and do not intend to use this
 commercially. We release rights with this installer to the user installing the software and take no responsibility
 of actions after install. 
 
