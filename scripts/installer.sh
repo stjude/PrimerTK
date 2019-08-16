7751a1d2dcb11abd89c3e901035b056efb0b575f
Dennis Kennetz <dennis.kennetz@stjude.org>
2019-08-16 13:20:43 -0500

    updated for primer3 to include all of package in copy and relevant ispcr compile info.

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -10,10 +10,13 @@
 ###########
 
 # Select the installation directory and make sure it exists.
-PRIMER3_INSTALL_DIR=~/bin/primer3
+PRIMER3_INSTALL_DIR=~/bin/
 mkdir -p $PRIMER3_INSTALL_DIR
 
 # Make a tmp location to download / install the source
+# we need the whole source to run the program. There are configs in
+# primer3_config that are referenced for thermodynamic params
+
 TMPDIR=$(mktemp -d)
 echo $TMPDIR
 cd $TMPDIR
@@ -23,31 +26,35 @@ make
 #make test
 
 # Copy the appropriate executables
-cp $TMPDIR/primer3/*.pl $PRIMER3_INSTALL_DIR
-cp $TMPDIR/src/primer3* $PRIMER3_INSTALL_DIR
-# TODO Any more needed?
+cp -rf $TMPDIR/primer3 $PRIMER3_INSTALL_DIR
 
 # Remove the tmp space
-#rm -rf $TMPDIR
+rm -rf $TMPDIR
 
 #########
 # isPCR #
 #########
 
 # Select the installation directory and make sure it exists
-ISPCR_INSTALL_DIR=~/bin/isPCR
+ISPCR_INSTALL_DIR=~/bin/isPcr33
 mkdir -p $ISPCR_INSTALL_DIR
-
 # Make a tmp location to download / install the source
 MACHTYPE=$(uname -m)
-TMPDIR=$(mktemp -d)
-echo $TMPDIR
-cd $TMPDIR
-wget https://hgwdev.gi.ucsc.edu/~kent/src/isPcr33.zip
-unzip isPcr33.zip 1> .unzip.out 2> .unzip.err
-cd $TMPDIR/isPcrSrc/lib && make HG_WARN=""
-cd $TMPDIR/isPcrSrc && make HG_WARN=""
+export MACHTYPE
+ISPCR_33=~/bin/$MACHTYPE
+mkdir -p $ISPCR_33
+export PATH=$PATH:$ISPCR_33
 
+TMPDIR2=$(mktemp -d)
+echo $TMPDIR2
+cd $TMPDIR2
+wget https://hgwdev.gi.ucsc.edu/~kent/src/isPcr33.zip && unzip isPcr33.zip 1> .unzip.out 2> .unzip.err
+mkdir ./isPcrSrc/lib/$MACHTYPE
+cd $TMPDIR2/isPcrSrc/lib && make HG_WARN=""
+cd $TMPDIR2/isPcrSrc && make HG_WARN=""
+cp $ISPCR_33/isPcr $ISPCR_INSTALL_DIR
+rm -rf $ISPCR_33
+rm -rf $TMPDIR2
 # TODO - why doesn't this compile?
-
+# there is code in the installer which specifically points to /usr/bin/$MACHTYPE as the place to put executables
 
